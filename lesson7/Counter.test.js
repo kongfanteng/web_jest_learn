9cd2fbfe08bdd45e28f34916b7ce7b153340a9fb
非凡腾达 <18210591009@163.com>
2024-05-06 11:03:32 +0800

    feat: lesson7 done

---
+++ lesson7/Counter.test.js
@@ -0,0 +1,41 @@
+import Counter from './Counter'
+describe('Counter 的测试代码', () => {
+  let counter = null
+  beforeAll(() => {
+    console.log('beforeAll')
+  })
+  beforeEach(() => {
+    console.log('beforeEach')
+    counter = new Counter()
+  })
+  afterEach(() => {
+    console.log('afterEach')
+  })
+  afterAll(() => {
+    console.log('afterAll')
+  })
+  describe('测试增加相关的代码', () => {
+    test('测试 Counter 中的 addOne 方法', () => {
+      console.log('测试 Counter 中的 addOne 方法')
+      counter.addOne()
+      expect(counter.number).toBe(1)
+    })
+    test('测试 Counter 中的 addTwo 方法', () => {
+      console.log('测试 Counter 中的 addTwo 方法')
+      counter.addTwo()
+      expect(counter.number).toBe(2)
+    })
+  })
+  describe('测试减少相关的代码', () => {
+    test('测试 Counter 中的 minusOne 方法', () => {
+      console.log('测试 Counter 中的 minusOne 方法')
+      counter.minusOne()
+      expect(counter.number).toBe(-1)
+    })
+    test('测试 Counter 中的 minusTwo 方法', () => {
+      console.log('测试 Counter 中的 minusTwo 方法')
+      counter.minusTwo()
+      expect(counter.number).toBe(-2)
+    })
+  })
+})
