cc28f171566f976c9415e39a9b6ff29524726a60
非凡腾达 <18210591009@163.com>
2024-05-06 18:31:48 +0800

    feat: lesson9 done

---
+++ lesson9/demo.js
@@ -0,0 +1,17 @@
+export const generateConfig = () => {
+  return {
+    server: 'http://localhost',
+    host: 8080,
+    domain: 'localhost',
+    timer: new Date(),
+  }
+}
+
+export const generateAnotherConfig = () => {
+  return {
+    server: 'http://localhost',
+    host: 8080,
+    domain: 'localhost',
+    timer: new Date(),
+  }
+}
