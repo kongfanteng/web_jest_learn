004c0c8e74d6bc53b750407794092545fe13ab2d
非凡腾达 <18210591009@163.com>
2024-05-06 10:51:43 +0800

    feat: lesson6 (2)

--- lesson6/fetchData.js
+++ lesson6/fetchData.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-// export function fetchData(fn) {
-//   return axios.get('https://api.github.com/users/gaearon/orgs')
-// }
-export function fetchData(fn) {
-  return axios.get('https://api.github.com/users/gaearon/orgsxxx')
+export function fetchData() {
+  return axios.get('http://www.dell-lee.com/react/api/demo.json')
+}
+export function fetchError() {
+  return axios.get('http://www.dell-lee.com/react/api/demo1.json')
 }
--- lesson6/fetchData.test.js
+++ lesson6/fetchData.test.js
@@ -1,19 +1,48 @@
-import { fetchData } from './fetchData'
+import { fetchData, fetchError } from './fetchData'
 
 // test('fetchData 返回结果为对象', (done) => {
 //   fetchData((data) => {
+//     console.log('data:', data)
 //     expect(typeof data).toBe('object')
 //     done()
 //   })
 // })
-// test('fetchData 返回结果为对象', () => {
-//   return fetchData().then(({ data }) => {
-//     expect(typeof data).toBe('object')
-//   })
-// })
-test('fetchData 返回结果为 404', () => {
+test('fetchData 返回结果为对象', () => {
+  return fetchData().then(({ data }) => {
+    expect(data).toEqual({ success: true })
+  })
+})
+test('fetchError 返回结果为 404', () => {
   expect.assertions(1)
-  return fetchData().catch((e) => {
+  return fetchError().catch((e) => {
     expect(e.toString().indexOf('404') > -1).toBe(true)
   })
+})
+test('fetchData 返回结果为 { success: true }', () => {
+  return expect(fetchData()).resolves.toMatchObject({
+    data: { success: true },
+  })
+})
+test('fetchError 返回结果为 404', () => {
+  return expect(fetchError()).rejects.toThrow()
+})
+test('fetchData 返回结果为 { success: true }', async () => {
+  await expect(fetchData()).resolves.toMatchObject({
+    data: { success: true },
+  })
+})
+test('fetchError 返回结果为 404', async () => {
+  await expect(fetchError()).rejects.toThrow()
+})
+test('fetchData 返回结果为 { success: true }', async () => {
+  const { data } = await fetchData()
+  expect(data).toEqual({ success: true })
+})
+test('fetchError 返回结果为 404', async () => {
+  expect.assertions(1)
+  try {
+    await fetchError()
+  } catch (error) {
+    expect(error.toString().indexOf('404') > -1).toBe(true)
+  }
 })
