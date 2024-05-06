34a47614aa287457e96870953236f99406dbed8d
非凡腾达 <18210591009@163.com>
2024-05-06 11:17:30 +0800

    feat: lesson7 作用域

--- lesson7/Counter.test.js
+++ lesson7/Counter.test.js
@@ -1,5 +1,6 @@
 import Counter from './Counter'
 describe('Counter 的测试代码', () => {
+  console.log('describe 1');
   let counter = null
   beforeAll(() => {
     console.log('beforeAll')
@@ -15,6 +16,13 @@ describe('Counter 的测试代码', () => {
     console.log('afterAll')
   })
   describe('测试增加相关的代码', () => {
+    console.log('describe 2')
+    beforeEach(() => {
+      console.log('beforeEach test add')
+    })
+    beforeAll(() => {
+      console.log('beforeAll test add')
+    })
     test('测试 Counter 中的 addOne 方法', () => {
       console.log('测试 Counter 中的 addOne 方法')
       counter.addOne()
@@ -27,6 +35,7 @@ describe('Counter 的测试代码', () => {
     })
   })
   describe('测试减少相关的代码', () => {
+    console.log('describe 3')
     test('测试 Counter 中的 minusOne 方法', () => {
       console.log('测试 Counter 中的 minusOne 方法')
       counter.minusOne()
