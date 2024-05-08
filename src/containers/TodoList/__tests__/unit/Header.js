fbf8707a2f734e14a8581635c0019af3d3b1d7ea
非凡腾达 <18210591009@163.com>
2024-05-08 12:48:25 +0800

    feat: vue6 done-通用代码提取封装

--- src/containers/TodoList/__tests__/unit/Header.js
+++ src/containers/TodoList/__tests__/unit/Header.js
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import Header from "../../components/Header.vue";
+import { findTestWrapper } from "@/utils/testUtils";
 
 it("Header 样式发生改变，进行提示", () => {
   const wrapper = shallowMount(Header);
@@ -8,7 +9,7 @@ it("Header 样式发生改变，进行提示", () => {
 
 it("Header 包含 input 框", () => {
   const wrapper = shallowMount(Header);
-  const input = wrapper.find("[data-test='input']");
+  const input = findTestWrapper(wrapper, "input");
   expect(input.exists()).toBe(true);
 });
 it("Header 中 input 框初始内容为空", () => {
@@ -18,28 +19,28 @@ it("Header 中 input 框初始内容为空", () => {
 });
 it("Header 中 input 框发送变化时，数据应该跟着变", () => {
   const wrapper = shallowMount(Header);
-  const input = wrapper.find("[data-test='input']");
+  const input = findTestWrapper(wrapper, "input");
   input.setValue("hello");
   const inputValue = wrapper.vm.$data.inputValue;
   expect(inputValue).toBe("hello");
 });
 it("Header 中 input 框输入回车，无内容，无反应", () => {
   const wrapper = shallowMount(Header);
-  const input = wrapper.find("[data-test='input']");
+  const input = findTestWrapper(wrapper, "input");
   input.setValue("");
   input.trigger("keyup.enter");
   expect(wrapper.emitted().add).toBeFalsy();
 });
 it("Header 中 input 框输入回车，有内容时，向外触发事件", () => {
   const wrapper = shallowMount(Header);
-  const input = wrapper.find("[data-test='input']");
+  const input = findTestWrapper(wrapper, "input");
   input.setValue("hello");
   input.trigger("keyup.enter");
   expect(wrapper.emitted().add).toBeTruthy();
 });
 it("Header 中 input 框输入回车，有内容时，向外触发事件，同时清空 inputValue", () => {
   const wrapper = shallowMount(Header);
-  const input = wrapper.find("[data-test='input']");
+  const input = findTestWrapper(wrapper, "input");
   input.setValue("hello");
   input.trigger("keyup.enter");
   expect(wrapper.emitted().add).toBeTruthy();
