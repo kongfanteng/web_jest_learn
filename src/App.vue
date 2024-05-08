ed6d84adee1e411c73ceb5d76e91d8fb3abf0f28
非凡腾达 <18210591009@163.com>
2024-05-08 11:58:12 +0800

    feat: vue5 done-Header 组件样式新增及快照测试

--- src/App.vue
+++ src/App.vue
@@ -12,3 +12,8 @@ export default {
   },
 };
 </script>
+<style lang="stylus">
+*
+  margin: 0
+  padding: 0
+</style>
--- src/containers/TodoList/components/Header.vue
+++ src/containers/TodoList/components/Header.vue
@@ -1,24 +1,29 @@
 <template>
-  <div>
-    <input
-      data-test="input"
-      type="text"
-      v-model="inputValue"
-      @keyup.enter="addTodoItem"
-    />
+  <div class="header">
+    <div class="header-content">
+      TodoList
+      <input
+        data-test="input"
+        type="text"
+        v-model="inputValue"
+        @keyup.enter="addTodoItem"
+        class="header-input"
+        placeholder="输入待办事项"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "TodoListHeader",
-  data () {
+  data() {
     return {
       inputValue: "",
     };
   },
   methods: {
-    addTodoItem () {
+    addTodoItem() {
       if (this.inputValue) {
         this.$emit("add", this.inputValue);
         this.inputValue = "";
@@ -27,3 +32,21 @@ export default {
   },
 };
 </script>
+<style scoped lang="stylus">
+.header
+  line-height 60px
+  background #334
+.header-content
+  width 600px
+  margin 0 auto
+  color #fff
+  font-size 24px
+.header-input
+  float right
+  width 360px
+  margin-top 16px
+  line-height 24px
+  outline none
+  color #333
+  text-indent 8px
+</style>
